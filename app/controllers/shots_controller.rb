2d54cb514ec373561388a775d670c7cbc2c272db
alauderdale <alauderdale@mac.com>
2013-06-07 01:03:24 -0600

    edit

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -8,7 +8,6 @@ class ShotsController < ApplicationController
     @shot = Shot.find(params[:id])
   end
 
-
   def new
     @shot = Shot.new
   end
@@ -22,6 +21,23 @@ class ShotsController < ApplicationController
     #if form fails, redisplay form so user can fix problems
     else
       render('new')
+    end
+  end
+
+  def edit
+    @shot = Shot.find(params[:id])
+  end
+
+  def update_shot
+    # Find object using form parameters
+    @shot = Shot.find(params[:id])
+    # Update the object
+    if @shot.update_attributes(params[:shot])
+      # If update succeeds, redirect to the list action
+      redirect_to shots_path, :notice => "Shot Updated"
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('edit')
     end
   end
 
