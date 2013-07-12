d39d18e9346d7c2bfb36b1683f07419f060a6f3a
alauderdale <alauderdale@mac.com>
2013-07-12 16:48:59 -0600

    attempt

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -1,7 +1,8 @@
 class Shot < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :dependent => :destroy
-  has_and_belongs_to_many :rebounds
+  has_many :rebounds
+  has_many :shots, :through => :rebounds
   has_attached_file :audio  ,
                     :url => "/assets/:class/:id/:attachment/:style.:extension",
                     :path => ":rails_root/public/assets/:class/:id/:attachment/:style.:extension"
