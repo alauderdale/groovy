8346664eb9906104b99a7d890cc5ace89b3b894b
alauderdale <alauderdale@mac.com>
2013-07-24 00:11:50 -0600

    likes

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -22,34 +22,6 @@ class ShotsController < ApplicationController
     end
   end
 
-
-  def rebound
-    if current_user
-      @rebound_shot = Shot.find(params[:id])
-      @shot = Shot.new
-    else
-      redirect_to new_user_session_path
-    end
-  end
-
-  def create_rebound
-    @shot = Shot.new(params[:shot])
-    @shot.user_id = current_user.id
-    @rebound_shot = Shot.find(params[:id])
-    if @shot.save
-    #If save succeeds return to list action
-      Rebound.create(:from_shot_id => @rebound_shot.id, :to_shot_id => @shot.id)
-      redirect_to shots_path, :notice => "Rebound Created"
-    #if form fails, redisplay form so user can fix problems
-    else
-      flash[:alert]= "Make sure the file you uploaded is an audio clip under 30 seconds!"
-      render :action => :new
-
-    end
-  end
-
-
-
   def create
     @shot = Shot.new(params[:shot])
     @shot.user_id = current_user.id
@@ -94,5 +66,19 @@ class ShotsController < ApplicationController
     shot = Shot.find(params[:id])
     shot.destroy
     redirect_to shots_path, :notice => "Shot Deleted"
+  end
+
+  def like_shot
+    shot = Shot.find(params[:id])
+    user = current_user
+    shot.liked_by user
+    redirect_to shot_path
+  end
+
+  def un_like_shot
+    shot = Shot.find(params[:id])
+    user = current_user
+    shot.disliked_by user
+    redirect_to shot_path
   end
 end
