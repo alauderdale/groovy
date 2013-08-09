a6a5abbdb67802a9b079dfff6a275942b205a181
alauderdale <alauderdale@mac.com>
2013-08-08 22:22:20 -0600

    cool

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
 	def index
-		@shots = Shot.search(params[:search]).page(params[:page]).order('created_at DESC')
+		@shots = Shot.fuzzy_search(params[:search]).page(params[:page]).order('created_at DESC')
 		@search = params[:search]
 	end
 end
