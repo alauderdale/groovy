128b86d84f2d8e037b8e73b7fec8943b9ee8a6d0
alauderdale <alauderdale@mac.com>
2013-08-08 20:41:10 -0600

    cool

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
 	def index
 		@shots = Shot.search(params[:search]).page(params[:page]).order('created_at DESC')
+		@search = params[:search]
 	end
 end
